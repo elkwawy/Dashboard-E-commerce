38096af2691547188f588a5aef7e2fe0c6cb781f
unknown <ama991969@gmail.com>
2024-11-20 22:22:13 +0200

    Updata Project

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,23 @@ import RootLayout from "./pages/RootLayout";
 import React, { Suspense } from "react";
 import Loader from "./utils/Loader";
 const Test1 = React.lazy(() => import("./pages/Test1"));
+const Dashboard = React.lazy(() => import("./pages/Dashboard"));
 const Test2 = React.lazy(() => import("./pages/Test2"));
 const NotFound = React.lazy(() => import("./pages/NotFound"));
+const Login = React.lazy(() => import("./Auth/Login"));
 
 
 // add routes here
 const routes = [ 
-  {path:'/', element:<Test1/>, /*isProtected:false */},
-  {path:'/2', element:<Test2/>, /*isProtected:false */},
+  {path:'', element:<Dashboard/>, /*isProtected:false */},
+  {path:'', element:<Test2/>, /*isProtected:false */},
+  {path:'/login', element:<Login/>, /*isProtected:false */},
   {path:'*', element:<NotFound />, /*isProtected:false */},
 ];
 
 const Router = createBrowserRouter(
   createRoutesFromElements(
-    <Route element={<RootLayout />}>
+    <Route  element={<RootLayout />}>
           {
             routes.map((route) => <Route key={route.path} path={route.path} element={route.element} />)
           }
--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -6,7 +6,7 @@ import { FaAngleRight } from "react-icons/fa6";
 import Loader from '../utils/Loader';
 
 const RootLayout = () => {
-    const [sidebar, setSidebar] = useState(false);
+    const [sidebar, setSidebar] = useState(true);
     const openSidebar  = () => setSidebar(true) ;
     const closeSidebar  = () => setSidebar(false) ;
 
@@ -69,12 +69,12 @@ const RootLayout = () => {
     return (
         <div className='w-full flex overflow-x-hidden'>
             <Sidebar sidebar={sidebar} closeSidebar={closeSidebar} baseLink={baseRealLink} subLink={subRealLink} lastLink={lastRealLink} />
-            <div className='flex w-full flex-col gap-5'>
+            <div className='flex w-full flex-col'>
                 <Navbar sidebar={sidebar} openSidebar={openSidebar} />
                 
                 {/* whatever put in the App Router comes here  */}
-                <div className=' flex flex-col gap-5 w-full   h-full'>
-                    <div className='px-2 min-[300px]:px-5 flex gap-1 sm:gap-2 text-lg items-center w-full max-[373px]:text-xs max-sm:text-sm'>
+                <div className=' flex flex-col gap-5 w-full bg-[#F5F6FA] pt-5  h-full'>
+                    <div className='px-2 min-[300px]:px-5  flex gap-1 sm:gap-2 text-lg items-center w-full max-[373px]:text-xs max-sm:text-sm'>
                         <NavLink to={'/'} className={({isActive}) => `${isActive ? "" : "text-sec-color"} trans hover:text-gray-600 `}>Dashboard</NavLink>
                         {baseLink && <div className='flex items-center gap-1 sm:gap-2  '> 
                             <FaAngleRight className='text-sec-color mt-[2px]' />  
@@ -85,13 +85,13 @@ const RootLayout = () => {
                             <span className={` outline-0 ${lastLink ? "text-sec-color " : " cursor-pointer trans hover:text-gray-600"}`}>{subLink}</span> 
                         </div>}
                         
-                        {lastLink && <div className='flex  items-center gap-1 sm:gap-2 w-fit'> 
+                        {lastLink && <div className='flex items-center gap-1 sm:gap-2 w-fit'> 
                             <FaAngleRight className='text-sec-color mt-[2px] ' />  
                             <span className='cursor-pointer w-fit'>{lastLink}</span> 
                         </div>}
                     </div>
-                    
-                    <div className={'px-2 min-[300px]:px-5 w-full h-full'}>
+                    {/*  dashboard content comes here  */}
+                    <div className={'px-2 min-[300px]:px-5 bg-[#F5F6FA] w-full h-full'}>
                         <Outlet /> 
                     </div>
                 </div>
